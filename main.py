b6d990a23bc71c9df50ea53821e5490a4113dbda
Vardges <sukiasyan.vardges@gmail.com>
2023-05-24 23:07:40 +0400

    Minor changes

--- main.py
+++ main.py
@@ -4,17 +4,23 @@ SECRET_KEY = '18afb34edaf10eaa2bfcf5f03106123e'
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/", methods=['GET'])
 @app.route("/home")
 def home_page():
     return "Welcome to homepage"
 
-@app.route('/register')
+
+@app.route('/register', methods=['POST'])
 def register_competition():
     if request.method != 'POST':
         err_resp = make_response('Method not allowed', 405)
         return err_resp
     else:
+        payload = request.json()
+        # TODO validate payload
+        # TODO encrypt and tokenize
+        # TODO send response
 
 
 if __name__ == '__main__':
