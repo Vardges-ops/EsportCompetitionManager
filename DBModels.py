7a1925ce4e7014dedd1506fd2acd435414c90f18
Vardges <sukiasyan.vardges@gmail.com>
2023-06-07 23:58:14 +0400

    Models FK attributes refactoring

--- DBModels.py
+++ DBModels.py
@@ -9,10 +9,10 @@ class GameType(Base):
     __tablename__ = 'gametype'
 
     Id = Column(Integer, primary_key=True)
-    game_type = Column(String)
+    type_name = Column(String)
     platform = Column(String)
 
-    games = relationship('Game', back_populates='game_type')
+    tournaments = relationship('Game', back_populates='category_type')
 
 
 class Game(Base):
@@ -20,7 +20,9 @@ class Game(Base):
 
     Id = Column(Integer, primary_key=True)
     name = Column(String)
+
     type_id = Column(Integer, ForeignKey('gametype.Id'))
+    category_type = relationship('GameType')
 
     tournaments = relationship("Tournament", back_populates='game')
 
@@ -30,8 +32,8 @@ class Tournament(Base):
 
     Id = Column(Integer, primary_key=True)
     name = Column(String)
-    start = Column(DateTime)
-    end = Column(DateTime)
+    start = Column(DateTime, nullable=True)
+    end = Column(DateTime, nullable=True)
 
     game_id = Column(Integer, ForeignKey('game.Id'))
     game = relationship("Game", back_populates='tournaments')
@@ -64,7 +66,7 @@ class TournamentPlace(Base):
     tournament = relationship("Tournament", back_populates="places")
 
     player_id = Column(Integer, ForeignKey('player.Id'))
-    player = relationship("Player", back_populates="tournaments")
+    player = relationship("Player", back_populates="place")
 
 
 if __name__ == "__main__":
