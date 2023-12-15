b2c4bf0e49da895edbfa76f5d441394aadb8b340
Vardges <sukiasyan.vardges@gmail.com>
2023-12-15 18:53:28 +0400

    Game insertion interface creation

--- TournamentProcessManagers.py
+++ TournamentProcessManagers.py
@@ -4,13 +4,18 @@ from .ModelsInterface import *
 DEFAULT_GAMETYPES = [{'game_type': 'Shooter', 'platform': 'PC'}, {'game_type': 'MMORPG', 'platform': 'PC'}]
 for gtype in DEFAULT_GAMETYPES:
     GameTypeInterface.add_gametype(**gtype)
-
 GameInterface.insert_game('Counter Strike', GameTypeInterface.get_gametype_by_name('Shooter').Id)
 GameInterface.insert_game('DOTA 2', GameTypeInterface.get_gametype_by_name('MMORPG'))
 
 
-class GameInsertionConsole:
-    pass
+class GameInsertionConsole(GameTypeInterface, GameInterface):
+
+    def insert_new_game(self, game_name, game_type_name):
+        gametype = self.get_gametype_by_name('Shooter')
+        if gametype is None:
+            raise ValueError('Game type not found, insert it first')
+        self.insert_game(game_name, self.get_gametype_by_name(game_type_name).Id)
+
 
 
 class TournamentInsertionConsole:
